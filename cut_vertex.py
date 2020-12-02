a2b9fae42a2375b22f2ffe8856df1383e8cd05ba
David Cabatingan <davidcabat@gmail.com>
2020-12-02 16:55:39 -0500

    update cut vertices

--- cut_vertex.py
+++ cut_vertex.py
@@ -18,18 +18,24 @@ class CutVertexGraph(Graph):
             (2, -2, 0)
         ]
 
-        self.edges = [(0,1), (1,2), (0,2), (2,3), (3,4), (2,4)]
+        self.edges = [(1,0), (2,1), (2,0), (2,3), (3,4), (2,4)]
 
 class CutVertexScene(OurGraphTheory):
     def construct(self):
         self.graph = CutVertexGraph()
         super().construct()
 
+        self.shift_graph(2*LEFT)
+
         self.draw(self.vertices)
         self.draw(self.edges)
         self.wait()
 
+        words = TextMobject("Not 2-connected.")
+        words.next_to(self.edges[4], RIGHT*5)
+    
         self.accent_vertices([self.vertices[2]])
 
         self.erase([self.vertices[2], self.edges[1], self.edges[2], self.edges[3], self.edges[5]])
+        self.play(Write(words))
         self.wait()
