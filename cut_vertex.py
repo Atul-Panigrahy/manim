baaf0f0310c8e0507f7ef232546a4d0a24cd27fb
David Cabatingan <davidcabat@gmail.com>
2020-12-04 13:57:24 -0500

    add to 2 connected definition

--- cut_vertex.py
+++ cut_vertex.py
@@ -15,10 +15,11 @@ class CutVertexGraph(Graph):
             (-2, 2, 0),
             (0, 0, 0),
             (2, 2, 0),
-            (2, -2, 0)
+            (2, -2, 0),
+            (0, 2, 0)
         ]
 
-        self.edges = [(1,0), (2,1), (2,0), (2,3), (3,4), (2,4)]
+        self.edges = [(1,0), (2,1), (2,0), (2,3), (3,4), (2,4), (5,1), (5,2), (5,3)]
 
 class CutVertexScene(OurGraphTheory):
     def construct(self):
@@ -29,13 +30,23 @@ class CutVertexScene(OurGraphTheory):
 
         self.draw(self.vertices)
         self.draw(self.edges)
+
+        connected = TextMobject("2-connected.")
+        connected.next_to(self.edges[4], RIGHT*8)
+        self.play(Write(connected))
         self.wait()
 
-        words = TextMobject("\\emph{Not} 2-connected.", color=RED)
-        words.next_to(self.edges[4], RIGHT*5)
+        self.erase([self.vertices[-1]] + self.edges[-3:])
+
+        not_connected = TextMobject("\\emph{Not} 2-connected.", color=RED)
+        not_connected.next_to(self.edges[4], RIGHT*8)
+
+        self.wait()
+
+        self.play(Transform(connected, not_connected))
     
         self.accent_vertices([self.vertices[2]])
 
         self.erase([self.vertices[2], self.edges[1], self.edges[2], self.edges[3], self.edges[5]])
-        self.play(Write(words))
+
         self.wait()
