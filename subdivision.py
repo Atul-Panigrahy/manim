5d89a3058531160114c82074ba474d6f47a1f7bb
PetarPeshev <petar_peshev@brown.edu>
2020-12-06 20:02:47 +0100

    spaced out subdividion and 2connected

--- subdivision.py
+++ subdivision.py
@@ -44,61 +44,86 @@ class PlanarSubgraphScene(OurGraphTheory):
         self.graph = K4()
         super().construct()
 
+        take_k4 = TextMobject("A graph is planar if some embedding of it \\\\ onto the plane has no edge intersections")
+        take_k4.shift(UP*3)
+        self.play(Write(take_k4))
+
         self.draw(self.vertices)
         self.draw(self.edges)
-        #self.wait()
+        self.wait(2)
 
 
         circle = Circle(radius=0.5, color=RED)
-        self.play(ShowCreation(circle, run_time=0.5))
-        self.play(FadeOut(circle, run_time=0.5))
-        #self.wait()
+        self.draw(circle, run_time=0.5)
+        self.erase_copy(circle, run_time=0.5, reverse=True)
+        self.wait(2)
 
         planar = OurGraphTheory(PlanarK4())
         planar.construct()
         graph_trans = zip(self.vertices + self.edges, planar.vertices+planar.edges)
         self.play(*[Transform(mobj1, mobj2) for mobj1,mobj2 in graph_trans])
-        #self.wait()
+        self.wait(5)
 
-        subgraph_words = TextMobject("Subgraphs of a planar graph are planar.")
-        subgraph_words.next_to(self.vertices[1], UP*2)
-        self.play(Write(subgraph_words))
+        subgraph_def = TextMobject("Subgraphs are subsets of vertices and edges")
+        subgraph_def.shift(UP*3)
+        self.play(ReplacementTransform(take_k4, subgraph_def))
 
         to_delete = [self.vertices[2]]
         to_delete += [self.edges[i] for i in range(1,len(self.edges))]
         self.erase_copy(to_delete)
-        #self.wait()
+        self.wait()
         to_return = [self.edges[4], self.edges[3]]
         self.draw(to_return)
-        #self.wait()
+        self.wait()
+
+        subgraph_planar = TextMobject("Subgraphs of planar graphs are planar")
+        subgraph_planar.shift(UP*3)
+        self.play(ReplacementTransform(subgraph_def, subgraph_planar))
+        self.wait()
         
         to_return = [self.vertices[2], self.edges[1], self.edges[2]]
         anims = self.draw(to_return, play=False)
         anims += self.erase_copy([self.edges[3]], play=False)
         self.play(*anims)
-        self.wait(2)
-
-        subdivision_words = TextMobject("Subdivisions of a planar graph are planar.")
-        subdivision_words.next_to(self.vertices[1], UP*2)
+        self.wait()
 
         anims = self.draw([self.edges[3], self.edges[5]], play=False)
-        anims += [Transform(subgraph_words, subdivision_words)]
         self.play(*anims)
+        self.wait(3)
 
+        subdivision_def = TextMobject("Subdivisions are obtained by replacing an edge \\\\ with 2 edges connected by a new vertex")
+        subdivision_def.shift(UP*3)
+        self.play(ReplacementTransform(subgraph_planar, subdivision_def))
+        self.wait()
+
+        self.erase_copy([self.edges[0]])
+        p = np.array((-1,0,0))
+        new_dot = Dot(p)
+        new_edges = [Line(planar.points[0], p),
+                    Line(planar.points[1], p)]
+        self.draw([new_dot]+new_edges)
+        self.wait(2)
+        self.erase_copy([new_dot])
+        
+        
+        subdivision_planar = TextMobject("Subdivisions of a planar graph are planar.")
+        subdivision_planar.shift(UP*3)
+
+        self.play(Transform(subdivision_def, subdivision_planar))
         self.wait()
 
         new_points = [
             random.uniform(0.15,0.85)*(planar.points[i]-planar.points[j]) + planar.points[j]
             for i, j in planar.edge_vertices
         ]
-        new_verts = [Dot(p, color=RED) for p in new_points]
+        new_verts = [Dot(p) for p in new_points]
         
         self.draw(new_verts)
-        self.wait()
+        self.wait(3)
         self.erase(new_verts)
         self.wait()
 
-        self.play(*(self.erase(self.vertices, play=False) + self.erase(self.edges, play=False)))
+        self.erase(self.vertices + self.edges + new_edges)
 
         return
 
@@ -131,45 +156,45 @@ class PlanarSubgraphScene(OurGraphTheory):
 
 
 
-class SubdivisionGraph(Graph):
-    def construct(self):
-        v1 = (-2, 0, 0)
-        v2 = (0, -2, 0)
-        v3 = (0, 2, 0)
-        v4 = (2, 0, 0)
-        v5 = (4, 0, 0)
-        self.vertices = [v1, v2, v3, v4, v5]
+# class SubdivisionGraph(Graph):
+#     def construct(self):
+#         v1 = (-2, 0, 0)
+#         v2 = (0, -2, 0)
+#         v3 = (0, 2, 0)
+#         v4 = (2, 0, 0)
+#         v5 = (4, 0, 0)
+#         self.vertices = [v1, v2, v3, v4, v5]
 
-        self.edges = [
-            (0, 1), 
-            (1, 3), 
-            (3, 2), 
-            (2, 0), 
-            (1, 4), 
-            (2, 4), 
-            (1, 2)
-        ]
+#         self.edges = [
+#             (0, 1), 
+#             (1, 3), 
+#             (3, 2), 
+#             (2, 0), 
+#             (1, 4), 
+#             (2, 4), 
+#             (1, 2)
+#         ]
 
 
 
-class PlanarSubdivisions(OurGraphTheory):
-    def construct(self):
-        self.graph = SubdivisionGraph()
-        super().construct()
-        self.draw(self.vertices)
-        self.draw(self.edges)
+# class PlanarSubdivisions(OurGraphTheory):
+#     def construct(self):
+#         self.graph = SubdivisionGraph()
+#         super().construct()
+#         self.draw(self.vertices)
+#         self.draw(self.edges)
 
-        new_points = [
-            random.uniform(0.15,0.85)*(self.points[i]-self.points[j]) + self.points[j]
-            for i, j in self.edge_vertices
-        ]
-        new_verts = [Dot(p, color=BLUE) for p in new_points]
+#         new_points = [
+#             random.uniform(0.15,0.85)*(self.points[i]-self.points[j]) + self.points[j]
+#             for i, j in self.edge_vertices
+#         ]
+#         new_verts = [Dot(p, color=BLUE) for p in new_points]
         
-        self.draw(new_verts)
-        self.wait()
-        self.erase(new_verts)
-        self.wait()
-        self.accent_vertices([self.vertices[4]])
-        self.erase([self.vertices[4]])
-        self.wait()
-        print(self.vertices[4].get_center())
+#         self.draw(new_verts)
+#         self.wait()
+#         self.erase(new_verts)
+#         self.wait()
+#         self.accent_vertices([self.vertices[4]])
+#         self.erase([self.vertices[4]])
+#         self.wait()
+#         print(self.vertices[4].get_center())
