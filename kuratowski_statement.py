cdcc842037a2a66979ccf50b9ce1f1150e7310a0
David Cabatingan <davidcabat@gmail.com>
2020-12-06 13:42:04 -0500

    statement of kuratowski thm

--- kuratowski_statement.py
+++ kuratowski_statement.py
@@ -11,32 +11,33 @@ from our_discrete_graph_scene import *
 class ForwardGraph(Graph):
     def construct(self):
         self.vertices = [
-            (0,0,0),
-            (1,2,0),
+            (-2,-1,0),
+            (1,-2,0),
             (2,1,0),
             (2,-1,0),
-            (1,-2,0),
             (-1,-2,0),
-            (-2,-1,0),
             (-2,1,0),
+            (0,0,0),
+            (-1,2,0),
+            (1,2,0),
             (-1,2,0),
         ]
 
         self.edges = [
-            (0,1),
-            (0,3),
-            (6,0),
-            (1,2),
+            (0,6),
+            (6,3),
+            (0,4),
+            (0,5),
+            (1,3),
+            (1,4),
+            (1,5),
             (2,3),
-            (4,3),
-            (4,5),
-            (6,5),
-            (6,7),
+            (2,4),
+            (2,5),
+            (5,7),
             (7,8),
-            (8,1),
-            (2,7),
-            (4,7),
-            (2,5)
+            (8,2),
+            (6,8)
         ]
 
 class K33TargetGraph(Graph):
@@ -54,10 +55,11 @@ class K33TargetGraph(Graph):
             (1,1,0),
             (1,0,0),
             (1,-1,0),
-            (0,-1,0),
+            (0,1,0),
         ]
         self.edges = [
-            (0,3),
+            (0,6),
+            (6,3),
             (0,4),
             (0,5),
             (1,3),
@@ -65,48 +67,60 @@ class K33TargetGraph(Graph):
             (1,5),
             (2,3),
             (2,4),
-            (2,6),
-            (6,5)
+            (2,5)
         ]
 
 class K33TargetScene(OurGraphTheory):
     def construct(self):
         self.graph = K33TargetGraph()
         super().construct()
-        self.shift_graph(RIGHT*4)
+        self.shift_graph(RIGHT*3)
 
 class KuratowskiStatementForwardScene(OurGraphTheory):
     def construct(self):
         self.graph = ForwardGraph()
         super().construct()
 
-        self.shift_graph(LEFT*4)
+        self.shift_graph(LEFT*3)
+
+        f1 = TextMobject("Kuratowski's Theorem: \\\\ A graph is nonplanar iff it has a subgraph \\\\ which is a subdivision of $K_5$ or $K_{3,3}$")
+        f1.shift(UP*3)
+        f1.scale(1)
+        self.play(Write(f1))
 
         self.play(*(self.draw(self.vertices, play=False) + self.draw(self.edges, play=False)))
         self.wait()
 
-        blue_verts = [2,6,4]
-        green_verts = [3,5,7]
+        f2 = TextMobject("Nonplanar graph $G$")
+        f2.shift(DOWN*3)
+        f2.scale(1)
+        self.play(Write(f2))
 
-        for v in blue_verts:
-            self.vertices[v].set_color(BLUE)
+        red_verts = [0,1,2]
+        green_verts = [3,4,5]
+
+        for v in red_verts:
+            self.vertices[v].set_color(RED)
 
         for v in green_verts:
             self.vertices[v].set_color(GREEN)
 
-        self.play(*[Transform(self.vertices[i], self.vertices[i]) for i in blue_verts + green_verts])
+        self.play(*[Transform(self.vertices[i], self.vertices[i]) for i in red_verts + green_verts])
 
         self.wait()
 
-        self.wait(2)
+        old_verts = self.vertices[:7]
+        old_edges = self.edges[:10]
 
-        old_verts = [self.vertices[i] for i in blue_verts + green_verts + [0]]
-        old_edges = [self.edges[i] for i in [4,13,11,2,1,7,8,5,6,12]]
-
-        remove_edges = [self.edges[i] for i in [0,3,9,10]]
-        remove_verts = [self.vertices[i] for i in [1,8]]  
+        remove_edges = self.edges[10:]
+        remove_verts = self.vertices[7:]  
 
         self.erase(remove_edges + remove_verts)
+
+        f3 = TextMobject("Subgraph of $G$")
+        f3.shift(DOWN*3)
+        f3.scale(1)
+        self.play(Transform(f2,f3))
 
         new_graph_scene = K33TargetScene()
 
@@ -115,4 +129,9 @@ class KuratowskiStatementForwardScene(OurGraphTheory):
 
         self.play(*(v_transforms + e_transforms))
 
-        self.wait()
+        f3 = TextMobject("Subgraph is a subdivison of $K_{3,3}$")
+        f3.shift(DOWN*3)
+        f3.scale(1)
+        self.play(Transform(f2,f3))
+
+        self.wait(2)
