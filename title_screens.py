a2d8d3525fd3696ff4d2f4dcf3199630adb578d8
PetarPeshev <petar_peshev@brown.edu>
2020-12-06 20:52:52 +0100

    title

--- title_screens.py
+++ title_screens.py
@@ -2,7 +2,18 @@ from manimlib.imports import *
 
 class IntroSlide(Scene):
     def construct(self):
-        "here"
+        title = TextMobject("Kurtowski's Theorem")
+        title.scale(2)
+
+        credit = TextMobject("by: David Cabatingan, Ken Cole, and Petar Peshev")
+        credit.scale(0.8)
+        credit.next_to(title, DOWN*2)
+        
+        self.play(Write(title))
+        self.play(Write(credit))
+        self.wait(5)
+        self.play(Uncreate(title), Uncreate(credit))
+        self.wait()
 
 class OutroSlide(Scene):
     def construct(self):
