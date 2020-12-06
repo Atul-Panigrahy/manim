9c1332cd9a1a155fd2aae10124dbbf2521faad1c
David Cabatingan <davidcabat@gmail.com>
2020-12-06 15:56:31 -0500

    fix outro slide

--- title_screens.py
+++ title_screens.py
@@ -19,10 +19,13 @@ class OutroSlide(Scene):
     def construct(self):
         credit = TextMobject("Credits").scale(1.5).to_edge(UP)
         credit_list = BulletedList(
-            "3b1b/manim: https://github.com/3b1b/manim",
-            "Mary Radcliffe: http://www.math.cmu.edu/~mradclif/teaching/228F16/Kuratowski.pdf",
-            "Artist: Music",
-            alignment="\\justify"
-        )
-        self.play(Write(credit), Write(credit_list))
+            " 3Blue1Brown: for the manim Python library",
+            " Mary Radcliffe: for the notes on Kuratowski's Theorem",
+            " Prof. Bena Tshishiku: for mentorship",
+            " Artist: for Music",
+        ).scale(0.8)
+        credit_links = TextMobject("https://github.com/3b1b/manim \\\\ http://www.math.cmu.edu/$\\sim$ mradclif/teaching/228F16/Kuratowski.pdf").next_to(credit_list, DOWN*4).scale(0.7)
+        self.play(Write(credit), Write(credit_list), Write(credit_links))
+        self.wait(5)
+        self.play(Uncreate(credit), Uncreate(credit_list), Uncreate(credit_links))
         self.wait()
